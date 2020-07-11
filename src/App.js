fbf8ee74df5c8bf58677d8bf0d91b1c994c8f39c
Plamen Shumanski <plamen1030@abv.bg>
2020-07-11 17:28:37 +0300

    feat: adding main script

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from './components/header'
 import styles from './app.module.css'
+import Aside from './components/aside'
+import Origamis from './components/origamis'
+
 
 function App() {
   return (
     <div className={styles.app}>
-    <Header/>
+      <Header />
+      <div>
+        <Aside />
+        <Origamis />
+      </div>
     </div>
   )
 }
---
+++ src/components/origamis/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styles from './index.module.css'
+
+class Origamis extends Component {
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <h1 className={styles.title}>Origamis</h1>
+                <div></div>
+            </div>
+        )
+    }
+}
+
+export default Origamis
