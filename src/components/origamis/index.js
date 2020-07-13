657e71229b909f250780de45d060890c79405810
Plamen Shumanski <plamen1030@abv.bg>
2020-07-13 14:03:10 +0300

    feat: adding origami code and fixing scripts

--- src/components/origamis/index.js
+++ src/components/origamis/index.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
+import Origami from '../origami'
 import styles from './index.module.css'
 
 class Origamis extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            origamis: []
+        }
+    }
+
+    getOrigamis = async () => {
+        const promise = await fetch('http://localhost:9999/api/origami')
+        const origamis = await promise.json()
+
+        this.setState({
+            origamis
+        })
+    }
+
+    componentDidMount() {
+        this.getOrigamis()
+    }
 
     render() {
+        const {
+            origamis
+        } = this.state
+
         return (
             <div className={styles.container}>
                 <h1 className={styles.title}>Origamis</h1>
-                <div></div>
+                <div className={styles['origamis-wrapper']}>
+                    {origamis.map(origami => {
+                        return (
+                            <Origami key={origami._id} {...origami}/>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
